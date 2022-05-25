81db1de5ce51b947ea128005750cdc8cb05c37c7
Никита Стецкий <stets119@gmail.com>
2022-05-25 13:13:25 +0300

    Origin allowed

--- src/main/java/by/grsu/scootersharing/controller/PersonController.java
+++ src/main/java/by/grsu/scootersharing/controller/PersonController.java
@@ -12,10 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
